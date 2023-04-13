8c92a5807eb7f4db3fe4bf1275152312c4efa23b
asalamgit <alisalamm95120@gmail.com>
2023-04-13 16:56:32 +0200

    avant migration de context a reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -6,6 +6,8 @@ export const UserContext = createContext({
   currentUser: null,
 });
 
+
+
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
