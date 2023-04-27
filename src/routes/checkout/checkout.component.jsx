0246a41705cc4a6adc98066cf835ccdc1304c1f0
asalamgit <alisalamm95120@gmail.com>
2023-04-27 18:22:46 +0200

    ajout de stripe

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
-
+import PaymentForm from "../../components/payment-form/payment-form.component";
 import './checkout.styles.scss';
 
 const Checkout = () => {
@@ -31,6 +31,7 @@ const Checkout = () => {
 				<CheckoutItem key={cartItem.id} cartItem={cartItem} />
 			))}
 			<div className="total">TOTAL: ${cartTotal}</div>
+			<PaymentForm />
 		</div>
 	);
 };
