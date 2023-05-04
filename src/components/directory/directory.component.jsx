d324fb1d1a7b9f7450e56e45d2394d0a9179fef0
asalamgit <alisalamm95120@gmail.com>
2023-05-04 16:56:04 +0200

    refonte css home

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -6,16 +6,24 @@ const Directory = ({ categories }) => {
 	return (
 		<div className="directory-container">
 			<div className="categories-line">
-				<CategoryItem key={categories[0].id} category={categories[0]} />
-				<CategoryItem key={categories[1].id} category={categories[1]} />
-				<CategoryItem key={categories[2].id} category={categories[2]} />
-				<CategoryItem key={categories[3].id} category={categories[3]} />
+				<div className="categories-line-column categories-line-left">
+					<CategoryItem key={categories[0].id} category={categories[0]} />
+					<CategoryItem key={categories[1].id} category={categories[1]} />
+				</div>
+				<div className="categories-line-column categories-line-right">
+					<CategoryItem key={categories[2].id} category={categories[2]} />
+					<CategoryItem key={categories[3].id} category={categories[3]} />
+				</div>
 			</div>
 			<div className="categories-line">
-				<CategoryItem key={categories[4].id} category={categories[4]} />
-				<CategoryItem key={categories[5].id} category={categories[5]} />
-				<CategoryItem key={categories[6].id} category={categories[6]} />
-				<CategoryItem key={categories[7].id} category={categories[7]} />
+				<div className="categories-line-column categories-line-left">
+					<CategoryItem key={categories[4].id} category={categories[4]} />
+					<CategoryItem key={categories[5].id} category={categories[5]} />
+				</div>
+				<div className="categories-line-column categories-line-right">
+					<CategoryItem key={categories[6].id} category={categories[6]} />
+					<CategoryItem key={categories[7].id} category={categories[7]} />
+				</div>
 			</div>
 		</div>
 	);
--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -2,19 +2,21 @@ import {  Fragment } from 'react';
 import CategoryPreview from '../../components/category-preview/category-preview.component';
 import { selectCategoriesMap } from '../../store/categories/categories.selector';
 import { useSelector } from 'react-redux';
+import './categories-preview.styles.scss'
 
 const CategoriesPreview = () => {
   const categoriesMap = useSelector(selectCategoriesMap);
 
   return (
-    <Fragment>
+    <div className='categories-preview'>
+      <img  className='categories-preview-img' src={"https://t3.ftcdn.net/jpg/02/89/30/62/360_F_289306227_kK8m66JgQfn7I71721LSpo8QvzpYBKrm.jpg"}/>
       {Object.keys(categoriesMap).map((title) => {
         const products = categoriesMap[title];
         return (
           <CategoryPreview key={title} title={title} products={products} />
         );
       })}
-    </Fragment>
+    </div>
   );
 };
 
