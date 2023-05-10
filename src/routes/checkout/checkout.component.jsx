e0a5213ffded9eaa96fe95070c976824acdd268e
asalamgit <alisalamm95120@gmail.com>
2023-05-10 18:07:47 +0200

    checkout item responsivite ok

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -10,28 +10,11 @@ const Checkout = () => {
 
 	return (
 		<div className="checkout-container">
-			{/* <div className="checkout-header">
-				<div className="header-block">
-					<span>Product</span>
-				</div>
-				<div className="header-block">
-					<span>Description</span>
-				</div>
-				<div className="header-block">
-					<span>Quantity</span>
-				</div>
-				<div className="header-block">
-					<span>Price</span>
-				</div>
-				<div className="header-block">
-					<span>Remove</span>
-				</div>
-			</div> */}
 			{cartItems.map((cartItem) => (
 				<CheckoutItem key={cartItem.id} cartItem={cartItem} />
 			))}
-			<div className="total">TOTAL: ${cartTotal}</div>
-			<PaymentForm />
+			<div className="total">TOTAL: {cartTotal} €</div>
+			{/* <PaymentForm /> */}
 		</div>
 	);
 };
